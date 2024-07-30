826f6a23dcccd65a9ad6caf52c55852dedcc7f24
unozer <40232537+unozer@users.noreply.github.com>
2024-07-30 17:27:10 +0200

    First Commit

---
+++ src/main/java/it/gp/rest_auth/RestAuthApplication.java
@@ -0,0 +1,13 @@
+package it.gp.rest_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gp/rest_auth/RestAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gp.rest_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
