54526207041bd2c4724a212804a42d0173bff4ae
unozer <40232537+unozer@users.noreply.github.com>
2024-07-30 17:41:18 +0200

    create Entity User e Role

---
+++ src/main/java/it/gp/rest_auth/model/RoleEntity.java
@@ -0,0 +1,21 @@
+package it.gp.rest_auth.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
---
+++ src/main/java/it/gp/rest_auth/model/UserEntity.java
@@ -0,0 +1,31 @@
+package it.gp.rest_auth.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<RoleEntity> roles = new ArrayList<>();
+}
