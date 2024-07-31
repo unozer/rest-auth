fe1072b175e213b796c11a0ecd9c3751b8fc7435
unozer <40232537+unozer@users.noreply.github.com>
2024-07-31 16:13:49 +0200

    svolti metodi Jwt Util

--- src/main/java/it/gp/rest_auth/utils/JwtUtil.java
+++ src/main/java/it/gp/rest_auth/utils/JwtUtil.java
@@ -1,26 +1,89 @@
 package it.gp.rest_auth.utils;
 
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.proc.BadJOSEException;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.text.ParseException;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JwtUtil {
 
     private static final int EXPIRE_HOUR_ACCESS_TOKEN = 24;
     private static final int EXPIRE_HOUR_REFRESH_TOKEN = 48;
 
+    private static final String SECRET = "pippo";
+
     public static String createAccessToken(String username, String issuer, List<String> roles) {
-        return "";
-        //TODO
+        try {
+            JWTClaimsSet claims = new JWTClaimsSet.Builder()
+                    .subject(username)
+                    .issuer(issuer)
+                    .claim("roles", roles)
+                    .expirationTime(Date.from(Instant.now().plusSeconds(EXPIRE_HOUR_ACCESS_TOKEN * 3600)))
+                    .issueTime(new Date())
+                    .build();
+
+            Payload payload = new Payload(claims.toJSONObject());
+
+            JWSObject jwsObject = new JWSObject(new JWSHeader(JWSAlgorithm.HS256), payload);
+
+            jwsObject.sign(new MACSigner(SECRET));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            throw new RuntimeException("Error creating JWT <createAccessToken>", e);
+        }
     }
 
     public static String createRefreshToken(String username) {
-        return "";
-        //TODO
+        try {
+            JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                    .subject(username)
+                    .expirationTime(Date.from(Instant.now().plusSeconds(EXPIRE_HOUR_REFRESH_TOKEN * 3600)))
+                    .build();
+
+            Payload payload = new Payload(claimsSet.toJSONObject());
+
+            JWSObject jwsObject = new JWSObject(new JWSHeader((JWSAlgorithm.HS256)), payload);
+
+            jwsObject.sign(new MACSigner(SECRET));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            throw new RuntimeException("Error creating JWT <createRefreshToken>", e);
+        }
     }
 
-    public static UsernamePasswordAuthenticationToken parseToken(String token) {
-        return null;
-        //TODO
+    public static UsernamePasswordAuthenticationToken parseToken(String token) throws ParseException, JOSEException, BadJOSEException {
+        SignedJWT signedJWT = SignedJWT.parse(token);
+        signedJWT.verify(new MACVerifier(SECRET));
+
+        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+
+        JWSKeySelector<SecurityContext> keySelector = new JWSVerificationKeySelector<>(JWSAlgorithm.HS256,
+                new ImmutableSecret<>(SECRET.getBytes()));
+        jwtProcessor.setJWSKeySelector(keySelector);
+        jwtProcessor.process(signedJWT, null);
+
+        JWTClaimsSet claims = signedJWT.getJWTClaimsSet();
+        String username = claims.getSubject();
+        List<String> roles = (List<String>)claims.getClaim("roles");
+
+        List<GrantedAuthority> authorities = roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        return new UsernamePasswordAuthenticationToken(username, null, authorities);
     }
 }
